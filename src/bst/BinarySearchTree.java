44436a09f0e7ad865217fed29eff0882bdc3e17c
Danny Tran <dannytran@Dannys-MacBook-Pro-M2-14.local>
2023-04-28 11:46:52 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -121,6 +121,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
+		if (node == null) {
+			return;
+		}
+		
+		System.out.print(node.data + " ");
+		
+		preOrderRecurse(node.leftChild);
+		
+		preOrderRecurse(node.rightChild);
 		
 	}
 	
